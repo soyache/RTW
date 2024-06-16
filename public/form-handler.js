b68ab7223a5678d7323ecc130afbb0a4a6c50e06
Hector Meza <hkmeza@gmail.com>
2024-06-16 09:51:48 -0600

    Refactor form submission logic

--- public/form-handler.js
+++ public/form-handler.js
@@ -1,41 +1,35 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   const scriptURL =
     "https://script.google.com/macros/s/AKfycbysuPXhjS_cVHj7ay8vCMldMXPTDaisZWuR2J6V1VwUHW3ibXXkd1INIJ7Zi1dLNo6OJA/exec";
-  const form = document.forms["bookingForm"];
+  const form = document.getElementById("bookingForm");
   const submitButton = form.querySelector('button[type="submit"]');
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    submitButton.disabled = true; // Deshabilita el botón
-    submitButton.textContent = "Loading..."; // Cambia el texto del botón
+    submitButton.disabled = true;
+    submitButton.textContent = "Loading...";
 
     fetch(scriptURL, {
       method: "POST",
       body: new FormData(form),
-      headers: {
-        Accept: "application/json",
-      },
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data); // Agrega esto para depuración
+        console.log(data);
         if (data.result === "success") {
-          // Solo redirige si la respuesta fue exitosa
           window.location.href = "/booking-confirmation";
         } else {
-          // Muestra un mensaje de error al usuario
           alert("There was an error submitting the form. Please try again.");
         }
-        form.reset(); // Reinicia el formulario
-        submitButton.disabled = false; // Rehabilita el botón
-        submitButton.textContent = "Submit"; // Restaura el texto del botón
+        form.reset();
+        submitButton.disabled = false;
+        submitButton.textContent = "Submit";
       })
       .catch((error) => {
-        console.error(error); // Agrega esto para depuración
-        // Muestra un mensaje de error al usuario
+        console.error(error);
         alert("There was an error submitting the form. Please try again.");
-        submitButton.disabled = false; // En caso de error, rehabilita el botón
-        submitButton.textContent = "Submit"; // Restaura el texto del botón
+        submitButton.disabled = false;
+        submitButton.textContent = "Submit";
       });
   });
 });
