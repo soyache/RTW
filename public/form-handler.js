b17f797a42a1f3461d8fa0976e3cd6de565cef09
Hector Meza <hkmeza@gmail.com>
2024-06-23 17:03:37 -0600

    Refactor form submission logic

--- public/form-handler.js
+++ public/form-handler.js
@@ -3,21 +3,14 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
   const form = document.getElementById("bookingForm");
   const scriptURL = form.getAttribute("data-script-url");
-  console.log("Script URL:", scriptURL); // Añadir log para depuración
-  if (!scriptURL) {
-    console.error("SCRIPT_URL is not defined");
-    return;
-  }
 
   const submitButton = form.querySelector('button[type="submit"]');
 
   form.addEventListener("submit", (e) => {
     e.preventDefault(); // Prevenir el comportamiento por defecto del formulario
-    console.log("Form submitted"); // Log para verificar el envío
 
     submitButton.disabled = true;
     submitButton.textContent = "Loading...";
-    console.log("Button disabled and text changed"); // Log para verificar el cambio de estado del botón
 
     fetch(scriptURL, {
       method: "POST",
@@ -27,25 +20,19 @@ window.addEventListener("DOMContentLoaded", (event) => {
       },
     })
       .then((response) => {
-        console.log("Response received"); // Log para verificar la recepción de la respuesta
         return response.json();
       })
       .then((data) => {
-        console.log("Response data:", data); // Log para verificar los datos de respuesta
         if (data.result === "success") {
-          console.log("Form submission successful, redirecting..."); // Log para verificar el éxito y la redirección
           window.location.href = "/booking-confirmation";
         } else {
-          console.log("Form submission failed, showing alert"); // Log para verificar la condición de fallo
           alert("There was an error submitting the form. Please try again.");
         }
         form.reset();
         submitButton.disabled = false;
         submitButton.textContent = "Submit";
-        console.log("Form reset and button re-enabled"); // Log para verificar el restablecimiento del formulario y el botón
       })
       .catch((error) => {
-        console.error("Error:", error); // Log para verificar errores en la captura
         alert("There was an error submitting the form. Please try again.");
         submitButton.disabled = false;
         submitButton.textContent = "Submit";
