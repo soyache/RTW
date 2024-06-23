bfb2afbc84bb9ba5a3365d901bdab6a109e7978f
Hector Meza <hkmeza@gmail.com>
2024-06-23 16:10:21 -0600

    Refactor form-handler.js to use import.meta.env and add logging

--- public/form-handler.js
+++ public/form-handler.js
@@ -1,14 +1,22 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  const scriptURL = process.env.SCRIPT_URL;
-  //  "https://script.google.com/macros/s/AKfycbysuPXhjS_cVHj7ay8vCMldMXPTDaisZWuR2J6V1VwUHW3ibXXkd1INIJ7Zi1dLNo6OJA/exec";
-  // process.env.SCRIPT_URL;
+  console.log("form-handler.js loaded"); // Log para verificar que el script se carga
+
+  const scriptURL = import.meta.env.SCRIPT_URL;
+  console.log("Script URL:", scriptURL); // Añadir log para depuración
+  if (!scriptURL) {
+    console.error("SCRIPT_URL is not defined");
+  }
+
   const form = document.getElementById("bookingForm");
   const submitButton = form.querySelector('button[type="submit"]');
 
   form.addEventListener("submit", (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+    console.log("Form submitted"); // Log para verificar el envío
+
     submitButton.disabled = true;
     submitButton.textContent = "Loading...";
+    console.log("Button disabled and text changed"); // Log para verificar el cambio de estado del botón
 
     fetch(scriptURL, {
       method: "POST",
@@ -17,20 +25,26 @@ window.addEventListener("DOMContentLoaded", (event) => {
         Accept: "application/json",
       },
     })
-      .then((response) => response.json())
+      .then((response) => {
+        console.log("Response received"); // Log para verificar la recepción de la respuesta
+        return response.json();
+      })
       .then((data) => {
-        console.log(data);
+        console.log("Response data:", data); // Log para verificar los datos de respuesta
         if (data.result === "success") {
+          console.log("Form submission successful, redirecting..."); // Log para verificar el éxito y la redirección
           window.location.href = "/booking-confirmation";
         } else {
+          console.log("Form submission failed, showing alert"); // Log para verificar la condición de fallo
           alert("There was an error submitting the form. Please try again.");
         }
         form.reset();
         submitButton.disabled = false;
         submitButton.textContent = "Submit";
+        console.log("Form reset and button re-enabled"); // Log para verificar el restablecimiento del formulario y el botón
       })
       .catch((error) => {
-        console.error(error);
+        console.error("Error:", error); // Log para verificar errores en la captura
         alert("There was an error submitting the form. Please try again.");
         submitButton.disabled = false;
         submitButton.textContent = "Submit";
